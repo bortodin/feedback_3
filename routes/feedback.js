8ddc6b93802d06329af9cbaa4eeb405989140949
bortodin <bortodinw@gmail.com>
2025-12-18 12:51:51 +0200

    Update feedback.js

--- routes/feedback.js
+++ routes/feedback.js
@@ -17,7 +17,7 @@ router.post('/', async (req, res) => {
         }
 
         // Check for existing feedback from this client in the last 60 minutes
-        const sixtyMinutesAgo = new Date(Date.now() - 10 * 10 * 1000);
+        const sixtyMinutesAgo = new Date(Date.now() - 1 * 1 * 1000);
         const existingFeedback = await Feedback.findOne({
             clientId: clientId,
             createdAt: { $gt: sixtyMinutesAgo }
@@ -125,4 +125,5 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
+
 
